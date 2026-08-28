6f79099abf5d48ca5f1364ed01bcfc728b644e37
Gerson Salomao <salomao.gersonneto@gmail.com>
2026-08-28 11:54:06 +0000

    Registro retroativo, voz do treinador, avisos de transicao e quinta como Base Cadencia
    - Marcar, editar e desfazer sessao em qualquer data pelo circulo do dia
    - Ofensiva, recorde, escudos e XP recalculados a partir das datas registradas
    - Player: tempo decorrido, barra de progresso da aula e do bloco
    - Aviso de transicao em tela cheia (ACELERA / AUMENTA A CARGA / SOLTA) com contagem
    - Voz do treinador em pt-BR e frases motivacionais por tipo de bloco
    - Quinta deixa de ser recuperacao e vira Base Cadencia, mesma duracao da terca
    - Botao para trocar a sessao do dia por recuperacao quando a perna estiver pesada

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "cadencia12-v1";
+const CACHE = "cadencia12-v3";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
